f9733476abd2740048ff2b63105702d0b7f802d3
Nicholas <nikolas.e@me.com>
2015-12-02 14:13:06 +0000

    comments added

--- app/src/main/java/com/example/nicholasesposito/courseapp/MainActivity.java
+++ app/src/main/java/com/example/nicholasesposito/courseapp/MainActivity.java
@@ -70,8 +70,8 @@ public class MainActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Bundle bundle = getIntent().getExtras();
-        category = bundle.getString("CATEGORY");
-        if (savedInstanceState != null)
+        category = bundle.getString("CATEGORY");//The String argument passed as extra in the intent gets retrieved and saved in the String variable category
+        if (savedInstanceState != null)//If the device change orientation, this is going to preserve data
         {index = savedInstanceState.getInt(KEY_INDEX);
             score = savedInstanceState.getInt(SCORE_VALUE);
             index --;
@@ -81,9 +81,11 @@ public class MainActivity extends FragmentActivity {
             index = 0;
             score = 0;
         }
+        //Facebook
         FacebookSdk.sdkInitialize(getApplicationContext());
         callbackManager = CallbackManager.Factory.create();
         shareDialog = new ShareDialog(this);
+        //Facebook
         question = (TextView) findViewById(R.id.lblQuestion);
         buttonTrue = (Button) findViewById(R.id.button_true);
         buttonFalse = (Button) findViewById(R.id.button_false);
@@ -96,41 +98,36 @@ public class MainActivity extends FragmentActivity {
             @Override
             public void onClick(View v) {
 
-                determineButtonPress(true);
+                determineButtonPress(true);//based on the pressed button the determineButtonPress method is called passing a boolean argument
             }
         });
-
         buttonFalse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 determineButtonPress(false);
             }
         });
-
         setUpQuestion();
-
-
     }
-
     private void setUpQuestion() {
-        ParseQuery<ParseObject> query = ParseQuery.getQuery("QuestionObject");
-        query.whereEqualTo("category", category);
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("QuestionObject");//The online parse database gets queried on the QuestionObject table
+        query.whereEqualTo("category", category);//The Question object table gets queried to retrive objects from the selected category
+        //the retrieved objects are put in a list of objects questionlist
         query.findInBackground(new FindCallback<ParseObject>() {
             public void done(List<ParseObject> questionlist, ParseException e) {
                 if (e == null) {
-                    if (index == questionlist.size())
+                    if (index == questionlist.size())//the game keeps going untill the last question in the list has been reached
                     {  endGame();}
                     else
                     {
-                    Log.d("score", "Retrieved " + questionlist.size() + " questions , index " + index);
-                    String questionText = questionlist.get(index).get("question").toString();
-                    ans = (boolean) questionlist.get(index).get("answ");
-                    question.setText(questionText);
+                    String questionText = questionlist.get(index).get("question").toString();//The question is retrieved from the given index of the list and put in a String variable
+                    ans = (boolean) questionlist.get(index).get("answ");//The answer is retrieved from the given index of the list and put in a boolean variable
+                    question.setText(questionText);//question is put in the TextView
                         question.setVisibility(View.VISIBLE);
                         buttonFalse.setVisibility(View.VISIBLE);
                         buttonTrue.setVisibility(View.VISIBLE);
-                        scoreView.setText(String.format("Score = %d", score));
-                    ParseFile fileObject = (ParseFile) questionlist.get(index).get("picture");
+                        scoreView.setText(String.format("Score = %d", score));//Score gets put in TextView
+                    ParseFile fileObject = (ParseFile) questionlist.get(index).get("picture");//A picture is retrieved from the database
                     fileObject.getDataInBackground(new GetDataCallback()
                     {
                         public void done(byte[] data, ParseException e)
@@ -138,10 +135,10 @@ public class MainActivity extends FragmentActivity {
                             if (e == null)
                             {
                                 Bitmap bmp = BitmapFactory.decodeByteArray(data, 0, data.length);
-                                roundedImage = new RoundImage(bmp);
-                                picture.setImageDrawable(roundedImage);
+                                roundedImage = new RoundImage(bmp);//image gets rounded
+                                picture.setImageDrawable(roundedImage);//and put in the ImageView
                                 picture.setVisibility(View.VISIBLE);
-                                findViewById(R.id.loadingPanel).setVisibility(View.GONE);
+                                findViewById(R.id.loadingPanel).setVisibility(View.GONE);//LoadingPanel is made invisible
                             }
                             else
                             {
@@ -158,26 +155,23 @@ public class MainActivity extends FragmentActivity {
 
 
     }
-
     private void determineButtonPress(boolean answer)
     {
         final boolean Answer = answer;
-        final boolean expectedAnswer = ans; //currentQuestion.isAnswer();
-        final Toast toast = Toast.makeText(MainActivity.this, "Correct!", Toast.LENGTH_SHORT);
-        final Toast toast1 = Toast.makeText(MainActivity.this, "Incorrect!",Toast.LENGTH_SHORT);
+        final boolean expectedAnswer = ans;
 
-        new CountDownTimer(1100, 1000) { // 5000 = 5 sec
+        new CountDownTimer(1100, 1000) { // a 1100 milliseconds timer is set
 
-            public void onTick(long millisUntilFinished) {
+            public void onTick(long millisUntilFinished) {//timer starts
                 buttonFalse.setVisibility(View.INVISIBLE);
                 buttonTrue.setVisibility(View.INVISIBLE);
                 question.setVisibility(View.INVISIBLE);
-                if(Answer == expectedAnswer)
+                if(Answer == expectedAnswer)//the answer gets evaluated
                 {
                     // toast.show();
-                    picture.setImageResource(R.drawable.ok);
-                    score ++;
-                    MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.correct);
+                    picture.setImageResource(R.drawable.ok);//an image is presented accordingly
+                    score ++;//if nswer is correct scoe is increased
+                    MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.correct);//a sound is played
                     mp.start();
                     scoreView.setText(String.format("Yeah!"));
                 }
@@ -194,43 +188,39 @@ public class MainActivity extends FragmentActivity {
 
             public void onFinish() {
                 setUpQuestion();
-            }
+            }//when timer finishes everything goes back to normal and the next question is presented
         }.start();
 
     }
 
     @Override
     protected void onSaveInstanceState(Bundle savedInstanceState) {
-        savedInstanceState.putInt(KEY_INDEX,index);
+        savedInstanceState.putInt(KEY_INDEX,index);//Data is saved if device gets turned
         savedInstanceState.putInt(SCORE_VALUE, score);
         super.onSaveInstanceState(savedInstanceState);
     }
     @Override
     protected void onResume() {
-        super.onResume();
-
+        super.onResume(); //Facebook analytics
         // Logs 'install' and 'app activate' App Events.
         AppEventsLogger.activateApp(this);
     }
     @Override
     protected void onPause() {
         super.onPause();
-
         // Logs 'app deactivate' App Event.
-        AppEventsLogger.deactivateApp(this);
+        AppEventsLogger.deactivateApp(this);//Facebook analytics
     }
-
     private void endGame() {
         final EditText input = new EditText(MainActivity.this);
-        final AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this)
+        final AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this)//An alert dialog is created at the end of the game
                 .setTitle("Congratulations")
                 .setMessage("You scored " + score + " points this round!")
-                .setView(input)
+                .setView(input)//user can input name
                 .setPositiveButton("save score", new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int whichButton) {
-
                                 m_Text = input.getText().toString();
-                                saveScore(m_Text);
+                                saveScore(m_Text);//score is saved in the local paper db and activitty ends
                                 finish();
                             }
                         }
@@ -238,7 +228,7 @@ public class MainActivity extends FragmentActivity {
                 )
                 .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int whichButton) {
-                                finish();
+                                finish();//score is not saved and activity ends
                             }
                         }
                 )
@@ -246,37 +236,28 @@ public class MainActivity extends FragmentActivity {
                             public void onClick(DialogInterface dialog, int whichButton) {
                                 if (ShareDialog.canShow(ShareLinkContent.class)) {
                                     ShareLinkContent linkContent = new ShareLinkContent.Builder()
-                                            .setContentTitle("My Score")
+                                            .setContentTitle("My Score")//User can share score on Facebook
                                             .setContentDescription(
                                                     "I scored " + score + " points in Nik's question App!")
                                             .setContentUrl(Uri.parse("https://github.com/Niko88/MobileDevCourseApp"))
                                             .build();
-
                                     shareDialog.show(linkContent);
                                     m_Text = input.getText().toString();
-                                    saveScore(m_Text);
+                                    saveScore(m_Text);//score is saved locally and activity ends
                                     finish();
                                 }
                             }
                         }
                 )
                 .create();
-
         alertDialog.show();
     }
-
-    public void saveScore(String m_Text)
+    public void saveScore(String m_Text)//scores get saved in local paper db
     {
-
-        //new high score
-        if(m_Text.length() == 0)
+        if(m_Text.length() == 0)//if no name has been inputted anon player gets saved as name
         {m_Text = "anon player";}
         HighScoreObject highScore = new HighScoreObject(score,m_Text, new Date().getTime());
-        //get user prefs
-
         List<HighScoreObject> highScores = Paper.book().read("highscores", new ArrayList<HighScoreObject>());
-
-        //add item
         highScores.add(highScore);
         Collections.sort(highScores, new Comparator<HighScoreObject>()
          {
@@ -295,9 +276,9 @@ public class MainActivity extends FragmentActivity {
                     return 0;
                 }
             }
-         });
+        });
 
-        Paper.book().write("highscores", highScores);
+        Paper.book().write("highscores", highScores);//scores are ordered in descending order and saved in local paper db
 
         score = 0;
     }
--- app/src/main/java/com/example/nicholasesposito/courseapp/category_picker.java
+++ app/src/main/java/com/example/nicholasesposito/courseapp/category_picker.java
@@ -15,16 +15,14 @@ public class category_picker extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_picker);
-        Button World;
-        Button Flags;
-        World = (Button) findViewById(R.id.worldButton);
-        Flags = (Button) findViewById(R.id.flagsButton);
+        Button World = (Button) findViewById(R.id.worldButton);
+        Button Flags = (Button) findViewById(R.id.flagsButton);
         Button back = (Button) findViewById(R.id.backButton);
 
         World.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-            StartIntent("world");
+            StartIntent("world");//The app based on the clicked button is going to call the StartIntent method passing a String argument
             }
         });
         Flags.setOnClickListener(new View.OnClickListener() {
@@ -36,7 +34,7 @@ public class category_picker extends AppCompatActivity {
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+                finish();//If the user press go back the activity gets stopped
             }
         });
 
@@ -48,7 +46,7 @@ public class category_picker extends AppCompatActivity {
     private void StartIntent(String category)
     {
         Intent i = new Intent(category_picker.this, MainActivity.class);//a new intent is created to connect this view  to the main activity
-        i.putExtra("CATEGORY", category);
+        i.putExtra("CATEGORY", category);//an intent to start main activity is created with an extra String argument
         startActivity(i);
     }
 
